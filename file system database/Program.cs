fcecfaf0350bd3d215fa88261a9765587599ee1e
Colin Behunin <colin.behunin@gmail.com>
2022-09-04 15:10:03 -0600

    Commit to make git happy

--- file system database/Program.cs
+++ file system database/Program.cs
@@ -5,11 +5,15 @@
 		static void Main(string[] args) {
 			DatabaseManager db = new("E:\\test.db");
 			//db.Create();
-			string[] drives = { "C:\\", "D:\\", "F:\\", "G:\\", "E:\\" };
+			string[] drives = { "C:\\", "D:\\", "F:\\", "G:\\", "E:\\"};
 			db.Update(drives, 0);
 			//db.RemoveFolder("C:\\Program Files");
 			//db.AddFolder("Z:\\",0);
 			//db.TestFunction("C:\\");
+			//List<(int, String, String, String, int, int)> values = db.FilesWithExtension(".txt");
+			//foreach ((int, String, String, String, int, int) t in values) {
+			//	Console.WriteLine(t);
+			//}
 			Console.WriteLine("blep");
 		}
 	}
