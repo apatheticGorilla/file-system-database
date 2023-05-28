015cb4fce9a479bf0722675b4dbfdc81fd3ebfe9
ApatheticGorilla <colin.behunin@gmail.com>
2023-05-28 12:52:35 -0600

    Completes #22

--- file system database/FileData.cs
+++ file system database/FileData.cs
@@ -23,6 +23,14 @@
 			extension = fi.Extension;
 		}
 
+		public FileData(string name, string path, int parentIndex, string extension, long size) {
+			this.name = name;
+			this.path = path;
+			this.parentIndex = parentIndex;
+			this.extension = extension;
+			this.size = size;
+		}
+
 		public string GetName() { return name; }
 		public string GetPath() { return path; }
 		public string GetExtension() { return extension; }
